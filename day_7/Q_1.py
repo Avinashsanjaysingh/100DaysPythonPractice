074119b877055376706f7792d085d56e18eb815c
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-04-14 16:01:16 +0530

    task done

--- day_7/Q_1.py
+++ day_7/Q_1.py
@@ -1,3 +1,5 @@
+# Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!
+
 m = int(input("Enter the first number : "))
 n = int(input("Enter the second Number : "))
 
