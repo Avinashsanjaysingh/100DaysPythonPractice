6e617f084675d814c173d073ae2972fd5c8a579e
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-04-17 08:01:32 +0530

    day 10 completed

--- day_010/main.py
+++ day_010/main.py
@@ -1,12 +1,14 @@
 
 
 a = input("Enter your name: ")
-print("My name is", a)
+print("My name is", a, type(a))
 
 x = input("Enter first number: ")
 y = input("Enter second number: ")
-print(x  + y)
+print(x + y)
 
 print(int(x) + int(y))
 
 print(float(x) + int(y))
+
+
