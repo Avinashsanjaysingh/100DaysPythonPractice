2c2acaf0154df3be7e931d1db59924ae622c98d7
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-04-23 09:23:06 +0530

    day 15 completed

--- day_015/main.py
+++ day_015/main.py
@@ -1,13 +1,15 @@
 
 
 import time
-timestamp = time.strftime('%H:%M:%S')
+timestamp = time.strftime('%H:%M:%S:%MS')
 print(timestamp)
 timestamp = time.strftime('%H')
 print(timestamp)
 timestamp = time.strftime('%M')
 print(timestamp)
 timestamp = time.strftime('%S')
+print(timestamp)
+timestamp = time.strftime('%MS')
 print(timestamp)
 # https://docs.python.org/3/library/time.html#time.strftime
 
