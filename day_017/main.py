128bbff106f1ba0148c199d0026f4953e3e0ec09
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-04-24 11:09:04 +0530

    day 17 completed (introducction to loops)

--- day_017/main.py
+++ day_017/main.py
@@ -1,25 +1,24 @@
 
-# name = 'Abhishek'
-# for i in name:
-#   print(i)
-#   if(i =="b"):
-#     print("This is something special!")
+name = 'Avinash'
+for i in name:
+  print(i)
+  if(i =="i"):
+    print("This is something special!")
     
-# colors = ["Red", "Green", "Blue", "Yellow"]
-# for color in colors:
-#   print(color)
-#   for i in color:
-#     print(i)
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
 
-# for k in range(5):
-#   print(k + 1)
+for k in range(5):
+  print(k + 1)
   
-# for k in range(1, 20001):
-#   print(k)
+for k in range(1, 21):
+  print(k)
 
   
 for k in range(1, 12, 3):
   print(k)
 
 
-  
