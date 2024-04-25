1504c2ea429ca041c69fffb593ec53bccea84b7c
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-04-25 09:26:47 +0530

    day 18 completed

--- day_018 (While Loops in Python)/main.py
+++ day_018 (While Loops in Python)/main.py
@@ -7,12 +7,12 @@ while(i<=38):
 
 print("Done with the loop")
 
-# count = 5
-# while (count > 0):
-#   print(count)
-#   count = count - 1
-# else:
-#   print("I am inside else")
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("I am inside else")
 
 # do {
   # loop body;
