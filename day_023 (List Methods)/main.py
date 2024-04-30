40717e2573417517527c79d93e841d48c09c0e33
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-04-30 14:21:53 +0530

    day 23 completed

--- day_023 (List Methods)/main.py
+++ day_023 (List Methods)/main.py
@@ -1,18 +1,31 @@
 
 l = [11, 45, 1, 2, 4, 6, 1, 1]
 print(l)
-# l.append(7)
-# l.sort(reverse=True)
-# l.reverse()
-# print(l.index(1))
-# print(l.count(1))
-# m = l.copy()
-# m[0] = 0
-# l.insert(1, 899)
+l.append(7)
+print(l)
+l.sort(reverse=True)
+print(l)
+l.reverse()
+print(l)
+print(l.index(1))   # It gives the index of "1"
+print(l.count(1))   # It gives the number of count of "1 "
+
+m = l.copy()
+print(m)
+
+m[0] = 0
+print(m,l)
+
+l.insert(1, 899)
+print(l)
+
 m = [900, 1000, 1100]
+print(l, m )
+
 k = l + m
-# print(k)
-# l.extend(m)
+print(k)
+
+l.extend(m) 
 print(l)
 
 
