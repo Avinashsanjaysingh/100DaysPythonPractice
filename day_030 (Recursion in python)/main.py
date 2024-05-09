c6983c19e1717019c3235adea912de26d1d3b09b
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-05-09 22:54:41 +0530

    day 30 completed

--- day_030 (Recursion in python)/main.py
+++ day_030 (Recursion in python)/main.py
@@ -21,6 +21,29 @@ print(factorial(5))
 # 5 * 4 * 3 * 2 * factorial(1)
 # 5 * 4 * 3 * 2 * 1
 
+
+def fact(num):
+  match num:
+    case 0: 
+      return 1
+    case 1: 
+      return 1
+    case _: 
+      return (num * fact(num-1))
+
+print(fact(5))
+
+def factorial(num): 
+    if (num == 1 or num == 0):
+        return 1
+    else:
+        return (num * factorial(num - 1)) 
+  
+# Driver Code 
+num = 7; 
+print("Number:",num)
+print("Factorial:",factorial(num))
+
 # Quick Quiz: Write a program to print the Fibonacci sequence
 # f(0) = 0
 # f(1) = 1
@@ -28,3 +51,43 @@ print(factorial(5))
 # f(n) = f(n-1) + f(n-2)
 
 # 0 1 1 2 3 5 8....
+
+def fib(n):
+  list = [0,1]
+  a = 0
+  b = 1
+  i = 0
+  
+  if n == 0:
+    list = []
+  elif n == 1:
+    list = [0]
+  elif n >= 2:
+    while (i < n-2):
+      b += a
+      a = b-a
+      list.append(b)
+      i +=1
+  print(list)
+
+print(fib(10))
+
+
+# def seq(n):
+#   num1 = 0
+#   num2 = 1
+#   next_number = num2 
+#   count = 1
+
+#   if n == 1:
+#     print('0', end='')
+#   else:
+#     print('0 1 ', end='')
+#   while count <= n-2:
+#     print(next_number, end=" ")
+#     count += 1
+#     num1, num2 = num2, next_number
+#     next_number = num1 + num2
+#   print()
+
+# print(seq(6))
