209ee2d0df45031af8770a234ed68517fb04c2a3
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-05-12 20:46:33 +0530

    day 31 completed

--- day_031 (Sets)/main.py
+++ day_031 (Sets)/main.py
@@ -1,15 +1,24 @@
 
-s = {2, 4, 2, 6}
+s = {2, 4, 2, 6, '2'}
 print(s)
 
 info = {"Carla", 19, False, 5.9, 19}
 print(info)
 
-harry = set()
-print(type(harry))
+avinash = {}
+print(avinash,type(avinash))
+
+avinash = set()
+print(type(avinash))
 
 for value in info:
   print(value)
 
 
-  
+info = {"Carla", 19, False, 5.9, 19, True}
+print(info)
+
+info = {"Carla", 19, False, 5.9}
+for item in info:
+    print(item)
+
