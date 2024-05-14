0d70520323a300e843d386ffc20bd687a806817c
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-05-14 10:50:55 +0530

    day 33 completed

--- day_033 (Dictionaries)/main.py
+++ day_033 (Dictionaries)/main.py
@@ -1,15 +1,34 @@
 
-info = {'name':'Karan', 'age':19, 'eligible':True}
-# print(info) 
-# print(info.keys())
-# print(info.values())
+#  python 3.7 onwards dictionary is ordered, before it was unordered.
 
-# for key in info.keys():
-#   print(f"The value corresponding to the key {key} is {info[key]}")
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info) 
+print(info.keys())
+print(info.values())
+
+for key in info.keys():
+  print(f"The value corresponding to the key {key} is {info[key]}")
 
 print(info.items())
 for key, value in info.items():
   print(f"The value corresponding to the key {key} is {value}") 
   
 
-  
+print()
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info)
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info['name'])
+print(info.get('eligible'))
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.values())
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.keys())
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.items())
+
