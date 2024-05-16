ac0c26d72adb564a590c6856565dde2874357b6f
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-05-16 11:14:26 +0530

    day 36 completed

---
+++ day_036 (Exception Handling)/more.py
@@ -0,0 +1,54 @@
+
+# Errors detected during execution are called exceptions
+
+# while True:
+#     try:
+#         x = int(input("Please enter a number: "))
+#         break
+#     except ValueError:
+#         print("Oops!  That was no valid number. Try again...")
+
+# except (RuntimeError, TypeError, NameError):
+#     pass
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except B:
+        print("B")
+    except D:
+        print("D")
+    except C:
+        print("C")
+
+
+
+try:
+    raise Exception('spam', 'eggs')
+except Exception as inst:
+    print(type(inst))    # the exception type
+    print(inst.args)     # arguments stored in .args
+    print(inst)          # __str__ allows args to be printed directly,
+                         # but may be overridden in exception subclasses
+    x, y = inst.args     # unpack args
+    print('x =', x)
+    print('y =', y)
