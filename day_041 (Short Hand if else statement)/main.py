254fa134f2aecdc7894781899c7f9847587fcff7
Avinash Singh <avinashsanjaysingh25@gmail.com>
2024-05-31 10:53:52 +0530

    day 41 completed

--- day_041 (Short Hand if else statement)/main.py
+++ day_041 (Short Hand if else statement)/main.py
@@ -1,7 +1,29 @@
 
-a = 330000
+a = 33000
 b = 3303
-print("A") if a > b else print("=") if a == b else print("B")
+print(a) if a > b else print("=") if a == b else print(b)
 
 c = 9 if a>b else 0
 print(c)
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+
+# Syntax
+
+'''
+result = value_if_true if condition else value_if_false
+
+# This syntax is equivalent to the following if-else statement:
+
+if condition:
+    result = value_if_true
+else:
+    result = value_if_false
+'''
